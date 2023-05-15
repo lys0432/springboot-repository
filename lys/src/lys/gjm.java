ba1fb11cdf8d8fa857dfb163c6f8c04469b142ff
lys0432 <lys0432@163.com>
2023-05-15 17:07:39 +0900

    first commit

---
+++ lys/src/lys/gjm.java
@@ -0,0 +1,14 @@
+package lys;
+
+public class gjm {
+	public static void main(String[] args)
+	{
+		System.out.print("Hello,World!!");
+		System.out.print("Hello,World!!");
+		System.out.print("Hello,World!!");
+		System.out.print("Hello,World!!");
+		System.out.print("Hello,World!!");
+		System.out.print("Hello,World!!");
+		System.out.print("Hello,World!!");
+	}
+}
